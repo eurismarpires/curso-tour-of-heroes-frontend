add6d04536efe36a645422bd14f5ac2fef3f0bfc
Eurismar Pires Borges <eurismarpires@gmail.com>
2024-11-02 00:13:32 -0300

    Limpa tela inicial e coloca as variáveis no html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'curso-tour-of-heroes-frontend';
+  nome = 'Eurismar Pires Borges';
+  hoje = new Date();
+
+
+
 }
